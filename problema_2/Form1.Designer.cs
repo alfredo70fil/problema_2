729aa5df0e916c653912f9390b5064ad4cb8696d
alfredo60fil <david.avila18@tectijuana.edu.mx>
2021-04-19 10:28:20 -0700

    actualización de codigo

--- problema_2/Form1.Designer.cs
+++ problema_2/Form1.Designer.cs
@@ -35,13 +35,14 @@ namespace problema_2
             this.btnSalida = new System.Windows.Forms.Button();
             this.listTabla = new System.Windows.Forms.ListBox();
             this.picPlato = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picPlato)).BeginInit();
             this.SuspendLayout();
             // 
             // btnMenu
             // 
             this.btnMenu.Location = new System.Drawing.Point(982, 38);
-            this.btnMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(2);
             this.btnMenu.Name = "btnMenu";
             this.btnMenu.Size = new System.Drawing.Size(56, 19);
             this.btnMenu.TabIndex = 0;
@@ -52,17 +53,18 @@ namespace problema_2
             // btnRecetas
             // 
             this.btnRecetas.Location = new System.Drawing.Point(982, 78);
-            this.btnRecetas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRecetas.Margin = new System.Windows.Forms.Padding(2);
             this.btnRecetas.Name = "btnRecetas";
             this.btnRecetas.Size = new System.Drawing.Size(56, 19);
             this.btnRecetas.TabIndex = 1;
             this.btnRecetas.Text = "Recetas";
             this.btnRecetas.UseVisualStyleBackColor = true;
+            this.btnRecetas.Click += new System.EventHandler(this.btnRecetas_Click);
             // 
             // btnAlmacen
             // 
             this.btnAlmacen.Location = new System.Drawing.Point(982, 126);
-            this.btnAlmacen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAlmacen.Margin = new System.Windows.Forms.Padding(2);
             this.btnAlmacen.Name = "btnAlmacen";
             this.btnAlmacen.Size = new System.Drawing.Size(56, 19);
             this.btnAlmacen.TabIndex = 2;
@@ -72,7 +74,7 @@ namespace problema_2
             // btnSalida
             // 
             this.btnSalida.Location = new System.Drawing.Point(982, 174);
-            this.btnSalida.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSalida.Margin = new System.Windows.Forms.Padding(2);
             this.btnSalida.Name = "btnSalida";
             this.btnSalida.Size = new System.Drawing.Size(56, 19);
             this.btnSalida.TabIndex = 3;
@@ -83,7 +85,7 @@ namespace problema_2
             // 
             this.listTabla.FormattingEnabled = true;
             this.listTabla.Location = new System.Drawing.Point(9, 10);
-            this.listTabla.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listTabla.Margin = new System.Windows.Forms.Padding(2);
             this.listTabla.Name = "listTabla";
             this.listTabla.Size = new System.Drawing.Size(773, 394);
             this.listTabla.TabIndex = 4;
@@ -92,29 +94,38 @@ namespace problema_2
             // 
             this.picPlato.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.picPlato.Location = new System.Drawing.Point(785, 15);
-            this.picPlato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picPlato.Margin = new System.Windows.Forms.Padding(2);
             this.picPlato.Name = "picPlato";
             this.picPlato.Size = new System.Drawing.Size(192, 186);
             this.picPlato.TabIndex = 5;
             this.picPlato.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(800, 264);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1078, 412);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.picPlato);
             this.Controls.Add(this.listTabla);
             this.Controls.Add(this.btnSalida);
             this.Controls.Add(this.btnAlmacen);
             this.Controls.Add(this.btnRecetas);
             this.Controls.Add(this.btnMenu);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picPlato)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,6 +137,7 @@ namespace problema_2
         private System.Windows.Forms.Button btnSalida;
         private System.Windows.Forms.ListBox listTabla;
         private System.Windows.Forms.PictureBox picPlato;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- problema_2/Form1.cs
+++ problema_2/Form1.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 namespace problema_2
 {
     public partial class Form1 : Form
     {
-
+        SqlConnection conexion = new SqlConnection("server=hikarinohakate.database.windows.net; Initial Catalog=Carta; integrated security=true");
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +26,11 @@ namespace problema_2
         private void btnMenu_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnRecetas_Click(object sender, EventArgs e)
+        {
+            SqlCommand altas = new SqlCommand("insert into  ()");
         }
     }
 }
